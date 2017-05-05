432c9320c6c6f44ea9f0add3a797b774cf7113ac
Иосиф Дзеранов <iosifdzeranov@Iosifs-MacBook-Air.local>
2017-05-05 12:24:36 +0300

    Implementation table view inset for nice for better visibility

--- Calculate-App-iOS/HistoryTableViewController.swift
+++ Calculate-App-iOS/HistoryTableViewController.swift
@@ -12,7 +12,7 @@ class HistoryTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.contentInset = UIEdgeInsets(top: 15, left: 0, bottom: 0, right: 0)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
